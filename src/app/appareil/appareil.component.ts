53fd077e321dd6e1f987c0c768ed332cb5ae4b51
CH4BRN <erreipantoine@gmail.com>
2020-07-07 16:48:41 +0200

    added binding

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-appareil',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppareilComponent implements OnInit {
 
+  @Input() appareilName: string = 'Machine';
+  @Input() appareilStatus: string = 'éteint';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getName(): string{
+    return this.appareilName;
+  }
+
+  getStatus(): string{
+    return this.appareilStatus;
+  }
+
+  getColor() {
+    if (this.appareilStatus === 'allumé') {
+      return 'green';
+    } else if (this.appareilStatus === 'éteint') {
+      return 'red';
+    }
+  }
 }
